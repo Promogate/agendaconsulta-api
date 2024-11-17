68978e7d8955971e0b606f9d9e8f4e2a4fda06e0
Augusto <gutodiasdev@gmail.com>
2024-11-17 10:55:16 -0300

    refact(repository): remove passwordHash from user return

--- src/domain/repositories/user-repository.ts
+++ src/domain/repositories/user-repository.ts
@@ -1,9 +1,9 @@
 import { User } from "../entities/user";
 
 export interface UserRepository {
-  create(user: Omit<User, "id" | "createdAt" | "updatedAt">): Promise<User>;
-  findById(id: string): Promise<User | null>;
-  findAll(): Promise<User[]>;
-  update(id: string, user: Partial<Omit<User, "id" | "createdAt" | "updatedAt">>): Promise<User | null>;
+  create(user: Omit<User, "id" | "createdAt" | "updatedAt">): Promise<Omit<User, "passwordHash">>;
+  findById(id: string): Promise<Omit<User, "passwordHash"> | null>;
+  findAll(): Promise<Omit<User, "passwordHash">[]>;
+  update(id: string, user: Partial<Omit<User, "id" | "createdAt" | "updatedAt">>): Promise<Omit<User, "passwordHash"> | null>;
   delete(id: string): Promise<boolean>;
 }
