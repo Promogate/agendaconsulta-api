0093c3803ff02b17e731158ca7a6db7daf21d2b7
Augusto <gutodiasdev@gmail.com>
2024-11-20 12:22:46 -0300

    feat(controller): add get all doctors route

--- src/application/controller/doctor-controller.ts
+++ src/application/controller/doctor-controller.ts
@@ -1,18 +1,28 @@
 import { Request, Response } from "express";
 import { HttpServer } from "../../infra/http/http-server";
 import { CreateDoctor } from "../../domain/features/doctor/create";
+import { FindAllDoctors } from "../../domain/features/doctor/find-all";
 
 export default class DoctorController {
   private namespace = "/doctors";
 
   constructor(
     readonly router: HttpServer,
-    readonly createDoctor: CreateDoctor
+    readonly createDoctor: CreateDoctor,
+    readonly findAll: FindAllDoctors
   ) {
     this.router.on("post", this.namespace, "/create", [], async (request: Request, response: Response) => {
       try {
         const result = await createDoctor.execute(request.body);
         response.status(201).json({ doctor: result });
+      } catch (error: any) {
+        response.status(400).json({ message: error.message });
+      }
+    });
+    this.router.on("get", this.namespace, "", [], async (request: Request, response: Response) => {
+      try {
+        const result = await findAll.execute();
+        response.status(200).json({ doctors: result });
       } catch (error: any) {
         response.status(400).json({ message: error.message });
       }
