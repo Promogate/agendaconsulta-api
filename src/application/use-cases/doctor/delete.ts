ebe9aecafd091b9b1ac00f597604d6e2895ecb04
Augusto <gutodiasdev@gmail.com>
2024-11-20 15:13:12 -0300

    refact(use-case): implements DeleteDoctor and add trycatch block

--- src/application/use-cases/doctor/delete.ts
+++ src/application/use-cases/doctor/delete.ts
@@ -1,9 +1,16 @@
+import { DeleteDoctor } from "../../../domain/features/doctor/delete";
 import { DoctorRepository } from "../../../domain/repositories/doctor-repository";
+import logger from "../../../lib/logger";
 
-export default class DeleteDoctorUseCase {
-  constructor(private doctorRepository: DoctorRepository) {}
+export default class DeleteDoctorUseCase implements DeleteDoctor {
+  constructor(private doctorRepository: DoctorRepository) { }
 
   async execute(id: string): Promise<void> {
-    return this.doctorRepository.delete(id);
+    try {
+      return this.doctorRepository.delete(id);
+    } catch (error: any) {
+      logger.error({ name: error.name }, error.message);
+      throw new Error(error.message);
+    }
   }
 }
