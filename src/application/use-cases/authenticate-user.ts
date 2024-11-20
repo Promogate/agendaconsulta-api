41cb93d4b42f25b64241b08dd1e9a5f00c8c50b4
Augusto <gutodiasdev@gmail.com>
2024-11-20 20:05:23 -0300

    feat(user-case): implement AuthenticateUser

---
+++ src/application/use-cases/authenticate-user.ts
@@ -0,0 +1,29 @@
+// src/application/usecases/AuthenticateUserUseCase.ts
+
+import { AuthenticateUser } from "../../domain/features/auth/authenticate-user";
+import { AuthRepository } from "../../domain/repositories/auth-repository";
+import logger from "../../lib/logger";
+
+export class AuthenticateUserUseCase implements AuthenticateUser {
+  constructor(private readonly authRepository: AuthRepository) { }
+
+  async execute(input: AuthenticateUser.Input): Promise<AuthenticateUser.Output> {
+    try {
+      const user = await this.authRepository.findUserByEmail(input.email);
+      if (!user) {
+        throw new Error("Invalid email or password.");
+      }
+      const isPasswordValid = await this.authRepository.validatePassword(input.password, user.passwordHash);
+      if (!isPasswordValid) {
+        throw new Error("Invalid email or password.");
+      }
+      const token = this.authRepository.generateToken(user.id);
+      return { token };
+    } catch (error: any) {
+      logger.error({
+        name: error.name
+      }, error.message);
+      throw new Error(error.stack);
+    }
+  }
+}
