aef0cc876ad0d74feabf88da2dce544a270d4109
Augusto <gutodiasdev@gmail.com>
2024-11-20 12:40:43 -0300

    feat(controller): add find by id route

--- src/application/controller/doctor-controller.ts
+++ src/application/controller/doctor-controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { HttpServer } from "../../infra/http/http-server";
 import { CreateDoctor } from "../../domain/features/doctor/create";
 import { FindAllDoctors } from "../../domain/features/doctor/find-all";
+import { FindById } from "../../domain/features/doctor/find-by-id";
 
 export default class DoctorController {
   private namespace = "/doctors";
@@ -9,7 +10,8 @@ export default class DoctorController {
   constructor(
     readonly router: HttpServer,
     readonly createDoctor: CreateDoctor,
-    readonly findAll: FindAllDoctors
+    readonly findAll: FindAllDoctors,
+    readonly findById: FindById
   ) {
     this.router.on("post", this.namespace, "/create", [], async (request: Request, response: Response) => {
       try {
@@ -22,6 +24,15 @@ export default class DoctorController {
     this.router.on("get", this.namespace, "", [], async (request: Request, response: Response) => {
       try {
         const result = await findAll.execute();
+        response.status(200).json({ doctors: result });
+      } catch (error: any) {
+        response.status(400).json({ message: error.message });
+      }
+    });
+    this.router.on("get", this.namespace, "/:id", [], async (request: Request, response: Response) => {
+      try {
+        const id = request.params.id;
+        const result = await findById.execute({ id });
         response.status(200).json({ doctors: result });
       } catch (error: any) {
         response.status(400).json({ message: error.message });
