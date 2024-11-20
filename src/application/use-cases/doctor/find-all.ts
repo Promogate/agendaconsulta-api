4967359418ff830f916946ba428e604816d04843
Augusto <gutodiasdev@gmail.com>
2024-11-20 12:20:58 -0300

    refact(use-case): implement FindAllDoctors interface and add a trycatch block

--- src/application/use-cases/doctor/find-all.ts
+++ src/application/use-cases/doctor/find-all.ts
@@ -1,10 +1,17 @@
 import { Doctor } from "../../../domain/entities/doctor";
+import { FindAllDoctors } from "../../../domain/features/doctor/find-all";
 import { DoctorRepository } from "../../../domain/repositories/doctor-repository";
+import logger from "../../../lib/logger";
 
-export default class FindAllDoctorsUseCase {
-  constructor(private doctorRepository: DoctorRepository) {}
+export default class FindAllDoctorsUseCase implements FindAllDoctors {
+  constructor(private doctorRepository: DoctorRepository) { }
 
   async execute(): Promise<Omit<Doctor, "password_hash">[]> {
-    return this.doctorRepository.findAll();
+    try {
+      return this.doctorRepository.findAll();
+    } catch (error: any) {
+      logger.error({ name: error.name }, error.message);
+      throw new Error(error.message);
+    }
   }
 }
