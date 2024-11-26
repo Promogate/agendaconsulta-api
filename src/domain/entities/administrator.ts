90120c4d7b1e064787f286fa93d4bb9abb861705
Augusto <gutodiasdev@gmail.com>
2024-11-26 18:51:27 -0300

    feat(entity): add Administrator

---
+++ src/domain/entities/administrator.ts
@@ -0,0 +1,15 @@
+export interface Administrator {
+  id: string;
+  name: string;
+  email: string;
+  password_hash: string;
+  admin_level: keyof typeof AdministratorLevel;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export enum AdministratorLevel {
+  admin = "ADMIN",
+  manager = "MANAGER",
+  employee = "EMPLOYEE",
+}
