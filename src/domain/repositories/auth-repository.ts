85224861aaedcb287f6bea6bc31261ebfe092332
Augusto <gutodiasdev@gmail.com>
2024-11-28 09:56:20 -0300

    feat(repository): add findAdministratorByEmail method to interface

--- src/domain/repositories/auth-repository.ts
+++ src/domain/repositories/auth-repository.ts
@@ -1,7 +1,13 @@
+import { Administrator } from "../entities/administrator";
 import { User } from "../entities/user";
 
 export interface AuthRepository {
   findUserByEmail(email: string): Promise<User | null>;
+  findAdministratorByEmail(email: string): Promise<FindAdministratorByEmail.Output>;
   validatePassword(password: string, hashedPassword: string): Promise<boolean>;
   generateToken(userId: string): string;
+}
+
+export namespace FindAdministratorByEmail {
+  export type Output = Omit<Administrator, "password_hash"> | null;
 }
