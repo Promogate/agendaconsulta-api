3c8b7280a2fdad2e47b8e37439e0842b73a781b5
Augusto <gutodiasdev@gmail.com>
2024-11-28 09:57:19 -0300

    refact(repository): adjust PrismaAdministratorRepository to recieve input.admin_level directly from input

--- src/infra/repositories/prisma/administrator-repository.ts
+++ src/infra/repositories/prisma/administrator-repository.ts
@@ -6,8 +6,7 @@ export class PrismaAdministratorRepository implements AdministratorRepository {
   constructor(private readonly prisma: PrismaClient) { }
 
   async create(input: CreateAdministratorDTO): Promise<CreateAdministrator.Output> {
-    const adminLevel = AdministratorLevel[input.admin_level as keyof typeof AdministratorLevel];
-    if (!adminLevel) {
+    if (!input.admin_level) {
       const admin = await this.prisma.administrator.create({
         data: {
           email: input.email,
@@ -29,7 +28,7 @@ export class PrismaAdministratorRepository implements AdministratorRepository {
         email: input.email,
         name: input.name,
         password_hash: input.password_hash,
-        admin_level: adminLevel
+        admin_level: input.admin_level
       }
     });
     return {
@@ -69,8 +68,7 @@ export class PrismaAdministratorRepository implements AdministratorRepository {
   }
 
   async update(id: string, data: UpdateAdministratorDTO): Promise<UpdateAdministrator.Output> {
-    const adminLevel = AdministratorLevel[data.admin_level as keyof typeof AdministratorLevel];
-    if (!adminLevel) {
+    if (!data.admin_level) {
       const admin = await this.prisma.administrator.update({
         where: { id },
         data: {
@@ -91,7 +89,7 @@ export class PrismaAdministratorRepository implements AdministratorRepository {
     const admin = await this.prisma.administrator.update({
       where: { id },
       data: {
-        admin_level: adminLevel,
+        admin_level: data.admin_level as keyof typeof AdministratorLevel,
         email: data.email,
         name: data.name,
         password_hash: data.password_hash
